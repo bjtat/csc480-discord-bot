6e1b6cebe19621c180248c0b4a3aad33d5419348
Brandon Tat <brandontat6@gmail.com>
2022-03-02 16:00:25 -0800

    working on cleaning up keywords

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ### How to set up bot for development
 - `pip install -r requirements.txt`
+- `python3 -m spacy download en_core_web_md`
 - Create a local file called .env that contains the TOKEN as an env variable
 - Token should look like `TOKEN={actual_token_here_and_remove_curly_brackets}`
 - `python3 main.py`
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import discord
 import os
 import spacy
 from dotenv import load_dotenv
-from Scripts import nlp_model
 from collections import Counter
 from string import punctuation
+
+from Scripts import nlp_model
 
 client = discord.Client()
 model = spacy.load('en_core_web_md')
@@ -23,9 +24,24 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('hello!')
 
-    if message.content.startswith('$robin'):
+    if message.content.startswith('Robin,'):
         await message.channel.send(f"message is: [{message.content}]")
-        await message.channel.send(f"keywords is: [{test_model(message.content)}]")
+        cleaned_msg = clean_msg(message.content)
+        keyword_list = test_model(cleaned_msg)
+        await message.channel.send(f"keywords is: [{keyword_list}]")
+        formatted = format_keywords(keyword_list)
+        await message.channel.send(formatted)
+
+def clean_msg(msg):
+    cm = msg.removeprefix("Robin,")
+    cm = cm.strip()
+    return cm
+
+def format_keywords(keywords):
+    formatted = ""
+    for count, keyword in enumerate(keywords):
+        formatted = formatted + f"{count}. {keyword}\n"
+    return formatted
 
 def test_model(msg):
     keywords = nlp_model.get_keywords(model, msg)

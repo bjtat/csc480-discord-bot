8033d2e1d610b7bb003742e2d5b3a208aaf325cc
jasonjtran <jtran200@calpoly.edu>
2022-03-09 02:15:40 -0800

    Add generated model output and load it in the basic NLP model script

--- Scripts/nlp_model.py
+++ Scripts/nlp_model.py
@@ -10,26 +10,20 @@ MIN_KEYWORDS = 10
 
 def get_keywords(model, text):
     result = []
-    pos_tag = ['VERB', 'ADJ', 'NOUN']
+    # pos_tag = ['VERB', 'ADJ', 'NOUN']
     doc = model(text.lower())
     for token in doc:
         if(token.text in model.Defaults.stop_words or token.text in punctuation):
             continue
-        if(token.pos_ in pos_tag):
-            result.append(token.text)
+        result.append(token.text)
                 
     return result
 
 
 if __name__ == '__main__':
-    sample_text = 'this is a sample sentence.'
-    user_input = input('Enter a sentence to extract keywords from: ')
+    sample_text = 'Robin, create a new branch please'
 
-    model = spacy.load('en_core_web_md')
+    model = spacy.load('./model_output/model-last')
 
-    input_text = user_input if user_input else sample_text
-    keywords = get_keywords(model, input_text)
-
-    print('\nTop 10 Keywords: \n')
-    sorted_keywords = [x[0] for x in Counter(keywords).most_common(min(MIN_KEYWORDS, len(keywords)))]
-    print(' '.join(sorted_keywords))
+    keywords = get_keywords(model, sample_text)
+    print(keywords)
